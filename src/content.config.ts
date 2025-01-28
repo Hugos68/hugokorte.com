b70a0b5edfc19cc787ecf0c7dd4feb254a96b90d
Hugos68 <hugokorteapple@gmail.com>
2025-01-28 19:09:18 +0100

    schema

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,14 +1,20 @@
 import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
-
+	type: 'content',
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		updatedAt: z.date(),
+		publishedAt: z.date().optional(),
+	})
 });
 
 const projects = defineCollection({
 	type: 'data',
 	schema: z.object({
 		title: z.string(),
-		content: z.string(),
+		description: z.string(),
 		preview: z.string().optional(),
 		source: z.string().optional(),
 	})
