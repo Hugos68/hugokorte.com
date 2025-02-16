e8057a283669a1987cefa9d840d94f007d69a39a
Hugos68 <hugokorteapple@gmail.com>
2025-02-16 13:58:55 +0100

    easier to read

--- src/pages/robots.txt/index.ts
+++ src/pages/robots.txt/index.ts
@@ -1,6 +1,11 @@
 import type { APIRoute } from "astro";
 
-const getRobotsTxt = (sitemap: URL) => `User-agent: *\nAllow: /\n\nSitemap: ${sitemap.href}`;
+const getRobotsTxt = (sitemap: URL) => [
+    "User-agent: *",
+    "Allow: /",
+    "\n",
+    `Sitemap: ${sitemap}`,
+].join("\n");
 
 export const GET: APIRoute = ({ site }) => {
 	const sitemapURL = new URL("sitemap-index.xml", site);
